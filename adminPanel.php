285f626432e6ccdb9940ef606a941f9bed4c3ada
walbyn <walbyngonzalez@gmail.com>
2025-10-24 15:18:17 -0600

    guardando cambio

--- adminPanel.php
+++ adminPanel.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 } 
 
-$rolFiltrado = $rolFiltrado ?? 'Administrador';
+$rolFiltrado = $rolFiltrado;
 $usuarios = listarUsuariosPorRol($rolFiltrado);
 
 ?>

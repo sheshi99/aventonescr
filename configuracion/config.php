53930ade4643df642a6c07a7150d4aded21785d5
Seidy <seidyalanis@gmail.com>
2025-10-23 22:46:46 -0600

    archivos con nuevas modificaciones

---
+++ configuracion/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('SMTP_USERNAME', '');
+
+define('SMTP_PASSWORD', ''); 
+
+// Otras configuraciones del correo
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_SECURE', 'tls');
+
+// Opcional: URL base del sitio
+define('BASE_URL', 'http://localhost/aventonescr');
+
+?>
